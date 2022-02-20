0d706c1e19de16457bc7f0aabd3cb30bdda33fda
Noah Geren <noah.s.geren@gmail.com>
2022-02-19 18:56:59 -0600

    Create Problem345.java

---
+++ src/com/noahgeren/projecteuler/problems/Problem345.java
@@ -0,0 +1,13 @@
+package com.noahgeren.projecteuler.problems;
+
+import com.noahgeren.projecteuler.utils.Problem;
+
+public class Problem345 extends Problem {
+
+	@Override
+	public String solve() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
